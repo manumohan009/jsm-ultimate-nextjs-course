a3fa2021d4a77fab41d341b324acce240d9911ec
Manu Mohanan <manu.mohanan@nationwidegroup.org>
2026-07-24 12:56:27 +0530

    Implement Right sidebar

--- context/Theme.tsx
+++ context/Theme.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { ThemeProviderProps } from "next-themes";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import React from "react";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
